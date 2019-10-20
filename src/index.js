7ee085b70d34916282e80681948a0828375fe7a9
Cedric B <cedricbardaine@gmail.com>
2019-10-20 14:37:55 +0200

    Actualising description and icons

--- src/index.js
+++ src/index.js
@@ -117,51 +117,55 @@ function Square(props) {
                         
                         let status ;
                         if ( winner ) { status =  winner + ' has won ! ' ;}
-                        else { status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O' ) ;  }
-                        
-                        
-                        return (
-                            <div className="game">
-                            <div className="game-board">
-                            <Board 
-                            squares={current.squares}
-                            onClick={(i) => this.handleClick(i)}
-                            />
-                            </div>
-                            <div className="game-info">
-                            <div>{status}</div>
-                            <ol>{moves}</ol>
-                            </div>
-                            </div>
-                            );
-                        }
-                    }
-                    
-                    function calculateWinner(squares) {
-                        const lines = [
-                            [0 , 1 ,2] , 
-                            [3, 4, 5],
-                            [6, 7, 8],
-                            [0, 3, 6],
-                            [1, 4, 7],
-                            [2, 5, 8],
-                            [0, 4, 8],
-                            [2, 4, 6],
-                        ];
-                        for( let i = 0 ; i < lines.length ; i++ ) {
-                            const [a , b , c] = lines[i] ; 
-                            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                                return squares[a];
+                        else { 
+                            if ( this.state.stepNumber == 9) { status = 'Nobody won this time.' ; }
+                            else { 
+                                status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O' ) ;  }
                             }
                             
+                            
+                            return (
+                                <div className="game">
+                                <div className="game-board">
+                                <Board 
+                                squares={current.squares}
+                                onClick={(i) => this.handleClick(i)}
+                                />
+                                </div>
+                                <div className="game-info">
+                                <div>{status}</div>
+                                <ol>{moves}</ol>
+                                </div>
+                                </div>
+                                );
+                            }
                         }
-                        return null;
-                    }
-                    
-                    // ========================================
-                    
-                    ReactDOM.render(
-                        <Game />,
-                        document.getElementById('root')
-                        );
                         
+                        function calculateWinner(squares) {
+                            const lines = [
+                                [0 , 1 ,2] , 
+                                [3, 4, 5],
+                                [6, 7, 8],
+                                [0, 3, 6],
+                                [1, 4, 7],
+                                [2, 5, 8],
+                                [0, 4, 8],
+                                [2, 4, 6],
+                            ];
+                            for( let i = 0 ; i < lines.length ; i++ ) {
+                                const [a , b , c] = lines[i] ; 
+                                if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                                    return squares[a];
+                                }
+                                
+                            }
+                            return null;
+                        }
+                        
+                        // ========================================
+                        
+                        ReactDOM.render(
+                            <Game />,
+                            document.getElementById('root')
+                            );
+                            
